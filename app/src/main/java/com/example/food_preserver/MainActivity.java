4e2e9bdcf005556399c048d409c27853972122d5
EarlGre <64865766+EarlGre@users.noreply.github.com>
2020-06-12 14:28:48 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/Tandrucson/Food-Preserver""
    This reverts commit 117f19275fa3ff5688883ad89a4307aab3b558b9.

--- app/src/main/java/com/example/food_preserver/MainActivity.java
+++ app/src/main/java/com/example/food_preserver/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
      String s1[], s2[];
      int images[] = {R.drawable.ic_carrot, R.drawable.ic_fruit, R.drawable.ic_meat};
 */
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
         searchIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), SearchActivity.class);
-                startIntent.putExtra("com.example.test.SOMETHING", "Hello world");
-                startActivity(startIntent);
+                // implement search activity here
+                //final Intent intentmethods;
+                //intentmethods = new Intent(MainActivity.this, Food_item_instructions.class);
+                Toast.makeText(MainActivity.this, "Go to Search Activity", Toast.LENGTH_SHORT).show();
+                //MainActivity.this.startActivity(intentmethods);
 
             }
         });
@@ -55,7 +60,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // implement ham activity here?
-                Toast.makeText(MainActivity.this, "Go to Hamburger Activity", Toast.LENGTH_SHORT).show();
+                final Intent intent;
+                intent = new Intent(MainActivity.this, Food_item_instructions.class);
+                //intent = new Intent(MainActivity.this, Menu.class);
+                Toast.makeText(MainActivity.this, "Menu", Toast.LENGTH_SHORT).show();
+                MainActivity.this.startActivity(intent);
+                Log.v("navhost", "issue");
             }
         });
 
